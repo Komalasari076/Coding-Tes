07832551b02c18435cbc0965b7ca507877359960
Komalasari076 <sitikomala076@gmail.com>
2024-09-03 19:36:28 +0700

    10 menit

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function menjumlahkanDataArray(arr) {
     arr = [];
+    arr = reduce();
 
     for (i = 0; i <= arr.legth; i++) {
-        
+        return arr(i);
     }
 }
+
+console.log(menjumlahkanDataArray([1, 2, 3]));
